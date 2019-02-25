6378ee0d375de48e59a782659cc4711de80bdb83
github username <github email address>
2019-02-25 18:22:49 -0500

    diff types of variables

--- hello-world.py
+++ hello-world.py
@@ -5,12 +5,16 @@ print(' /  |')
 print('/___|')
 
 # variables in python
+# strings 
 character_name = 'Tom'
-character_age = '50'
+#numbers or decimal num's
+character_age = 50
+#boolean value T/F value
+is_Male = True
 print('There once was a man named ' + character_name + ', ')
 print('he was ' + character_age + ' years old. ')
 
-#changing the variable value 
+#changing the variable value
 character_name = "Mike"
 print('He really liked the name ' + character_name +', ')
 print('but didnt like being ' + character_age + '. ')
