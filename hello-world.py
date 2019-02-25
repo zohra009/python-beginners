024a81da7901cea20bc18355e7c315dde9a251e4
github username <github email address>
2019-02-25 18:05:59 -0500

    variables

--- hello-world.py
+++ hello-world.py
@@ -1,5 +1,13 @@
-
+# order of instruction in python matter!
 print('   /|')
 print('  / |')
 print(' /  |')
 print('/___|')
+
+# variables in python
+character_name = 'John'
+character_age = '35'
+print('There once was a man named' + character_name + ', ')
+print('he was 70 years old. ')
+print('He really liked the name George, ')
+print('but didnt like being 70. ')
