2f0293088e2ccbb1ede7123acac0d1e3148a09dd
github username <github email address>
2019-02-25 18:19:17 -0500

    updating varibles

--- hello-world.py
+++ hello-world.py
@@ -5,9 +5,12 @@ print(' /  |')
 print('/___|')
 
 # variables in python
-character_name = 'John'
-character_age = '35'
+character_name = 'Tom'
+character_age = '50'
 print('There once was a man named ' + character_name + ', ')
 print('he was ' + character_age + ' years old. ')
+
+#changing the variable value 
+character_name = "Mike"
 print('He really liked the name ' + character_name +', ')
 print('but didnt like being ' + character_age + '. ')
