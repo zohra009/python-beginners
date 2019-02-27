964dfb94c768ea4e2fcf979d4d60c65bf9ec6e02
github username <github email address>
2019-02-27 00:08:52 -0500

    idex and length

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+#concatination = taking a string and appending another string together
 phrase =  "Giraffe Academy"
-print(phrase)
+print(phrase.upper())
+print(phrase.lower())
+#string with index on string 0 being first letter
+print(phrase[0])
+# length function to get length of string
+print(len(phrase))
+print(phrase.upper().isupper()) #isupper gives t/f value
+print(phrase.isupper())
 print ('Giraffe\nAcademy') #\n creates new line
+#functions can modify string and get info on strings
