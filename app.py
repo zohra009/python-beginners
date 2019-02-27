2fb1eb2e6f72d3f276d9c41f69eb6f93ed564599
github username <github email address>
2019-02-27 00:13:24 -0500

    replace

--- app.py
+++ app.py
@@ -2,8 +2,12 @@
 phrase =  "Giraffe Academy"
 print(phrase.upper())
 print(phrase.lower())
+#replace's giraffe with elephant 
+print(phrase.replace('Giraffe', 'Elephant'))
 #string with index on string 0 being first letter
-print(phrase[0])
+print(phrase[3])
+#index function gives position of the value in #'s
+print(phrase.index('Acad'))
 # length function to get length of string
 print(len(phrase))
 print(phrase.upper().isupper()) #isupper gives t/f value
