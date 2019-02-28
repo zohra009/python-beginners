095b41e216083a92d744c0c0cd00d15f24890a08
github username <github email address>
2019-02-27 19:02:01 -0500

    math in py

--- num.py
+++ num.py
@@ -1,3 +1,13 @@
 
 print(-2.0987)
 print(3 + 4.5)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+#modulous divide #'s and spit out remainder
+print(10 % 3)
+#store them in variables
+my_num = 5
+#print num next to a string use str(#)
+print(str(my_num) + " my favorite number")
+#common math functions
+print(abs(my_num))
