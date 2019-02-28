14733e16b3fd8a03f00f51dacaffa87dbf733e9e
github username <github email address>
2019-02-27 19:09:31 -0500

    math import

--- num.py
+++ num.py
@@ -1,3 +1,5 @@
+#the import allows us to do more math functions
+from math import *
 
 print(-2.0987)
 print(3 + 4.5)
@@ -9,5 +11,8 @@ print(10 % 3)
 my_num = 5
 #print num next to a string use str(#)
 print(str(my_num) + " my favorite number")
-#common math functions abs(), pow(#, power raised to), max(#1, #2)
-print(max(4,6)) #gives larger of the two #
+#common math functions abs(), pow(#, power raised to), max(#1, #2), min(#1, #2)
+print(floor(4.6))
+print(ceil(4.3))
+print(sqrt(49))
+print(round(4.4))
