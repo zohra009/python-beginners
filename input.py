d628f55a680ab1f4cf443554ffda96f054f495e7
github username <github email address>
2019-03-06 23:20:16 -0500

    input and debugging

---
+++ input.py
@@ -0,0 +1,6 @@
+#tell py it's getting input from user
+age =  input('Enter your age: ')
+name =  input('Enter your name: ')
+
+print('Hello ' + name + '! You are ' + age)
+#time 52mins
--- user-input.py
+++
@@ -1,6 +0,0 @@
-#tell py it's getting input from user
-name =  input("Enter your name: ")
-age =  input("Enter your age: ")
-
-print("Hello " + name + "! You are " + age)
-#time 52mins
